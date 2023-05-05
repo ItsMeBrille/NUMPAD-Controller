09272b9d0573e69444080a20ff4448097a428225
ItsMeBrille <herhellum@gmail.com>
2023-05-06 00:07:22 +0200

    changed scan-codes to fit my numpad

--- main.py
+++ main.py
@@ -2,69 +2,86 @@ import keyboard
 
 def keyPressed():
     while True:
-        # NumLock
-        if keyboard.is_pressed(69):
-            return [0, 0]
+        ### Row 0 ##
         # /
         if keyboard.is_pressed(53):
             return [0, 1]
 
         # *
-        if keyboard.is_pressed(55):
+        elif keyboard.is_pressed(55):
             return [0, 2]
 
         # -
-        if keyboard.is_pressed(74):
+        elif keyboard.is_pressed(74):
             return [0, 3]
+        
 
+        ### Row 1 ##
         # NumPad 7
-        if keyboard.is_pressed(71):
+        elif keyboard.is_pressed(71):
             return [1, 0]
 
         # NumPad 8
-        if keyboard.is_pressed(72):
+        elif keyboard.is_pressed(72):
             return [1, 1]
 
         # NumPad 9
-        if keyboard.is_pressed(73):
+        elif keyboard.is_pressed(73):
             return [1, 2]
 
         # +
-        if keyboard.is_pressed(78):
+        elif keyboard.is_pressed(78):
             return [1, 3]
 
+
+        ### Row 2 ##
         # NumPad 4
-        if keyboard.is_pressed(75):
+        elif keyboard.is_pressed(75):
             return [2, 0]
 
         # NumPad 5
-        if keyboard.is_pressed(76):
+        elif keyboard.is_pressed(76):
             return [2, 1]
 
         # NumPad 6
-        if keyboard.is_pressed(77):
+        elif keyboard.is_pressed(77):
             return [2, 2]
+        
+        # Backspace
+        elif keyboard.is_pressed(14):
+            return [2, 3]
+        
 
+        ### Row 3 ###
         # NumPad 1
-        if keyboard.is_pressed(79):
+        elif keyboard.is_pressed(79):
             return [3, 0]
 
         # NumPad 2
-        if keyboard.is_pressed(80):
+        elif keyboard.is_pressed(80):
             return [3, 1]
 
         # NumPad 3
-        if keyboard.is_pressed(81):
+        elif keyboard.is_pressed(81):
             return [3, 2]
+        
+        # Enter
+        elif keyboard.is_pressed(28):
+            return [3, 3]
 
+
+        ### Row 4 ###
         # NumPad 0
-        if keyboard.is_pressed(82):
+        elif keyboard.is_pressed(82):
+            return [4, 0]
+
+        # NumPad 000
+        elif keyboard.is_pressed(11):
             return [4, 1]
 
         # Decimal (,)
-        if keyboard.is_pressed(83):
+        elif keyboard.is_pressed(83):
             return [4, 2]
-    
+
 while True:
-    print(keyPressed())
-    break
+    print( keyPressed() )
