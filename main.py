ec9da964dd311230fec0e7e4aae049819928dccc
ItsMeBrille <herhellum@gmail.com>
2023-05-06 15:51:56 +0200

    restructure in main

--- main.py
+++ main.py
@@ -1,87 +1,20 @@
 import keyboard
 
 def keyPressed():
+    ### Row 0 ##
+    numpad = {
+        53 : [0, 1], 55 : [0, 2], 74 : [0, 3], # /, *, -
+        71 : [1, 0], 72 : [1, 1], 73 : [1, 2], 78 : [1, 3], # 7, 8 ,9, +
+        75 : [2, 0], 76 : [2, 1], 77 : [2, 2], 14 : [2, 3], # 4, 5, 6, Backspace
+        79 : [3, 0], 80 : [3, 1], 81 : [3, 2], 28 : [3, 3], # 1, 2, 3, Enter
+        82 : [4, 0], 11 : [4, 1], 83 : [4, 2] # 0, 000, Decimal
+    }
+    
+    for key in numpad:
+        if keyboard.is_pressed(key):
+            return numpad[key]
+    return False
+
+if __name__ == "__main__":
     while True:
-        ### Row 0 ##
-        # /
-        if keyboard.is_pressed(53):
-            return [0, 1]
-
-        # *
-        elif keyboard.is_pressed(55):
-            return [0, 2]
-
-        # -
-        elif keyboard.is_pressed(74):
-            return [0, 3]
-        
-
-        ### Row 1 ##
-        # NumPad 7
-        elif keyboard.is_pressed(71):
-            return [1, 0]
-
-        # NumPad 8
-        elif keyboard.is_pressed(72):
-            return [1, 1]
-
-        # NumPad 9
-        elif keyboard.is_pressed(73):
-            return [1, 2]
-
-        # +
-        elif keyboard.is_pressed(78):
-            return [1, 3]
-
-
-        ### Row 2 ##
-        # NumPad 4
-        elif keyboard.is_pressed(75):
-            return [2, 0]
-
-        # NumPad 5
-        elif keyboard.is_pressed(76):
-            return [2, 1]
-
-        # NumPad 6
-        elif keyboard.is_pressed(77):
-            return [2, 2]
-        
-        # Backspace
-        elif keyboard.is_pressed(14):
-            return [2, 3]
-        
-
-        ### Row 3 ###
-        # NumPad 1
-        elif keyboard.is_pressed(79):
-            return [3, 0]
-
-        # NumPad 2
-        elif keyboard.is_pressed(80):
-            return [3, 1]
-
-        # NumPad 3
-        elif keyboard.is_pressed(81):
-            return [3, 2]
-        
-        # Enter
-        elif keyboard.is_pressed(28):
-            return [3, 3]
-
-
-        ### Row 4 ###
-        # NumPad 0
-        elif keyboard.is_pressed(82):
-            return [4, 0]
-
-        # NumPad 000
-        elif keyboard.is_pressed(11):
-            return [4, 1]
-
-        # Decimal (,)
-        elif keyboard.is_pressed(83):
-            return [4, 2]
-
-while True:
-    print( keyPressed() )
+        print( keyPressed() )
